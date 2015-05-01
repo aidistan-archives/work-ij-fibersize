dd79b0b2b1d16964aebd0b25c187f4653ebb5195
Aidi Stan <aidistan@live.com>
2015-05-01 19:55:30 +0800

    Implement PlugInFilter instead

--- lib/Cell_Segmentation.java
+++ lib/Cell_Segmentation.java
@@ -1,72 +1,30 @@
 import ij.*;
 import ij.gui.*;
+import ij.process.*;
 import ij.plugin.frame.*;
+import ij.plugin.filter.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class Cell_Segmentation extends PlugInFrame {
-
-  public final int WINDOW_WIDTH  = 140;
-  public final int WINDOW_HEIGHT = 90;
+public class Cell_Segmentation implements PlugInFilter {
 
   // External
+  public ImagePlus  imp;
   public RoiManager roiMgr;
 
-  // Internal
-  static PlugInFrame instance;
-
-  public Cell_Segmentation() {
-    super("Cell Seg");
+  public int setup(String arg, ImagePlus imp) {
+    this.imp = imp;
 
     // Open ROI Manager
     roiMgr = RoiManager.getInstance();
     if (roiMgr == null)
       roiMgr = new RoiManager();
 
-    // Open Cell Seg
-    if (instance != null)
-      instance.toFront();
-    else {
-      WindowManager.addWindow(this);
-      setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-      GUI.center(this);
-      setVisible(true);
-      instance = this;
-    }
+    return DOES_ALL;
   }
 
-  // Overrides Component.addNotify(). getInsets() must be called after
-  // addNotify() or it will not return the title bar height.
-  public void addNotify() {
-    super.addNotify();
-
-    int xm = 20; // x margin
-    int ym = getInsets().top + 25; // y margin
-    int yw = 25;
-    int yi = 10;
-
-    setLayout(null);
-    setForeground(java.awt.Color.darkGray);
-    setResizable(false);
-
-    Button segmentBtn = new Button("Segment");
-    // segmentBtn.addActionListener(null);
-    // segmentBtn.addKeyListener(null);
-    segmentBtn.setName("segment");
-    segmentBtn.setBounds(xm, ym, WINDOW_WIDTH - xm*2, yw);
-    add(segmentBtn);
-
-    // Button filterBtn = new Button("Filter");
-    // filterBtn.addActionListener(null);
-    // filterBtn.addKeyListener(null);
-    // filterBtn.setName("filter");
-    // filterBtn.setBounds(xm, ym + (yw + yi), WINDOW_WIDTH - xm*2, yw);
-    // add(filterBtn);
+  public void run(ImageProcessor ip) {
+    // Put this line to validate
+    ip.invert();
   }
 
-  // Overrides windowClosing in PluginFrame.
-  public void windowClosing(WindowEvent e) {
-    super.windowClosing(e);
-    instance = null;
-  }
 }
