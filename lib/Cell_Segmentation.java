af118edd023f0057a0b2bfd3c2e0d29f116b4667
Aidi Stan <aidistan@live.com>
2015-05-03 01:39:39 +0800

    Finish threshold part

--- lib/Cell_Segmentation.java
+++ lib/Cell_Segmentation.java
@@ -21,8 +21,14 @@ public class Cell_Segmentation extends PlugInFrame {
   public final int WINDOW_HEIGHT = 280;
 
   // Internal
-  static PlugInFrame instance; // Singleton
-  ParticleAnalyzer pa;
+  public static PlugInFrame instance; // Singleton
+  public ImagePlus inspectation;
+  public ParticleAnalyzer pa;
+
+  // Controls
+  public Scrollbar thresholdBar;
+  public TextField erodeField;
+  public Checkbox inspectationChkbox;
 
   public Cell_Segmentation() {
     super("Cell Segmentation");
@@ -31,19 +37,16 @@ public class Cell_Segmentation extends PlugInFrame {
       return;
     } else {
       WindowManager.addWindow(this);
-      instance = this;
       setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
       GUI.center(this);
       setVisible(true);
 
+      // Initialize
+      instance = this;
+      thresholdBar.setValue(get_current_ip().getAutoThreshold());
+      inspectation = null;
       pa = new ParticleAnalyzer();
     }
-  }
-
-  /* Overrides windowClosing in PluginFrame. */
-  public void windowClosing(WindowEvent e) {
-      super.windowClosing(e);
-      instance = null;
   }
 
   /* Overrides Component.addNotify(). */
@@ -53,9 +56,6 @@ public class Cell_Segmentation extends PlugInFrame {
     super.addNotify();
     int top = getInsets().top + 32;
     Label label;
-    Scrollbar sclbar;
-    TextField txtfield;
-    Checkbox chkbox;
     Button button;
 
     /*
@@ -72,24 +72,54 @@ public class Cell_Segmentation extends PlugInFrame {
     label.setBounds(20, top + 30, 70, 14);
     add(label);
 
-    sclbar = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, 0, 255);
-    sclbar.setBounds(100, top + 28, WINDOW_WIDTH - 120, 16);
-    add(sclbar);
+    thresholdBar = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, 0, 255);
+    thresholdBar.setBounds(100, top + 28, WINDOW_WIDTH - 120, 16);
+    thresholdBar.addAdjustmentListener(new AdjustmentListener() {
+      public void adjustmentValueChanged(AdjustmentEvent e) {
+        if (inspectation == null) return;
+        if (e.getValueIsAdjusting()) return;
+        inspectation.close();
+        inspectation = new ImagePlus(get_binary_title(), get_binary_ip());
+        inspectation.show();
+        inspectation.updateAndDraw();
+      }
+    });
+    add(thresholdBar);
 
     label = new Label("Erode Iteration", Label.LEFT);
     label.setFont(new Font("Helvetica", Font.PLAIN, 12));
     label.setBounds(20, top + 60, 100, 14);
     add(label);
 
-    txtfield = new TextField("0", 3);
-    txtfield.setFont(new Font("Helvetica", Font.PLAIN, 12));
-    txtfield.setBounds(130, top + 60, 30, 14);
-    add(txtfield);
+    erodeField = new TextField("0", 3);
+    erodeField.setFont(new Font("Helvetica", Font.PLAIN, 12));
+    erodeField.setBounds(130, top + 60, 30, 14);
+    add(erodeField);
 
-    chkbox = new Checkbox("Inspectation", false);
-    chkbox.setFont(new Font("Helvetica", Font.PLAIN, 12));
-    chkbox.setBounds(170, top + 60, WINDOW_WIDTH - 180, 14);
-    add(chkbox);
+    inspectationChkbox = new Checkbox("Inspectation", false);
+    inspectationChkbox.setFont(new Font("Helvetica", Font.PLAIN, 12));
+    inspectationChkbox.setBounds(170, top + 60, WINDOW_WIDTH - 180, 14);
+    inspectationChkbox.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        if (e.getStateChange() == ItemEvent.SELECTED ) {
+          inspectation = new ImagePlus(get_binary_title(), get_binary_ip());
+          inspectation.show();
+          inspectation.updateAndDraw();
+          inspectation.addImageListener(new ImageListener() {
+            public void imageClosed(ImagePlus imp) {
+              inspectationChkbox.setState(false);
+              inspectation = null;
+            }
+            public void imageOpened(ImagePlus imp) {}
+            public void imageUpdated(ImagePlus imp) {}
+          });
+        } else {
+          inspectation.close();
+          inspectation = null;
+        }
+      }
+    });
+    add(inspectationChkbox);
 
     /*
      * Detection
@@ -103,7 +133,7 @@ public class Cell_Segmentation extends PlugInFrame {
     button = new Button("Particles Analysis Parameter");
     button.setFont(new Font("Helvetica", Font.PLAIN, 12));
     button.setBounds(30, top + 125, WINDOW_WIDTH - 60, 24);
-    button.addActionListener(new ActionListener(){
+    button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) { pa.showDialog(); }
     });
     add(button);
@@ -127,4 +157,37 @@ public class Cell_Segmentation extends PlugInFrame {
     setResizable(false);
   }
 
+  /* Overrides windowClosing in PluginFrame. */
+  public void windowClosing(WindowEvent e) {
+      super.windowClosing(e);
+      instance = null;
+  }
+
+  // Get a copy of the current IP in Gray8 format
+  public ImageProcessor get_current_ip() {
+    ImagePlus imp = IJ.getImage();
+    int oriC = imp.getC();
+    imp.setC(imp.getNChannels());
+    ImageProcessor ip = imp.getChannelProcessor();
+    ImageProcessor ip_ = ip.convertToByte(true);
+    imp.setC(oriC);
+    return(ip_);
+  }
+
+  // Get the title of a binary IP
+  public String get_binary_title() {
+    return(
+      "Inspectation (" +
+      "threshold " + Integer.toString(thresholdBar.getValue()) +
+      ", erode " + Integer.toString(Integer.parseInt(erodeField.getText())) +
+      ")"
+    );
+  }
+
+  // Get a binary IP (maybe for inspectation)
+  public ImageProcessor get_binary_ip() {
+    ImageProcessor ip = get_current_ip();
+    ip.threshold(thresholdBar.getValue());
+    return(ip);
+  }
 }
