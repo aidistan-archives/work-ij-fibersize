edc0bd7528dac03ec4487626ebaaa9d32859e792
Aidi Stan <aidistan@live.com>
2015-05-05 14:08:52 +0800

    Refactor further

--- lib/Fiber_Size.java
+++ lib/Fiber_Size.java
@@ -1,6 +1,7 @@
 import ij.*;
 import ij.gui.*;
 import ij.process.*;
+import ij.measure.*;
 import ij.plugin.filter.*;
 import ij.plugin.frame.*;
 import java.awt.*;
@@ -19,21 +20,26 @@ public class Fiber_Size extends PlugInFrame {
 
   // Constant
   public final int WINDOW_WIDTH = 480;
-  public final int WINDOW_HEIGHT = 250;
+  public final int WINDOW_HEIGHT = 360;
 
   // External
-  public ImagePlus _imp;
+  public ImagePlus orgImp;
 
   // Internal
   public static PlugInFrame instance; // Singleton
-  public ImageListener impLst;
-  public ImageProcessor ip;
-  public ImagePlus imp;
+  public ImageListener imgLtn;
+  public ImagePlus bndImp;
+  public ImageProcessor bndIp;
 
   // Controls
-  public TextField imgField;
-  public Scrollbar sclBar;
-  public TextField thdField;
+  public int yPos;
+  public TextField imgFld;
+  public Scrollbar bndSclBar;
+  public TextField bndThdFld;
+  public Choice chnChoice;
+  public Choice typChoice;
+  public Scrollbar typSclBar;
+  public TextField typThdFld;
 
   public Fiber_Size() {
     super("Fiber Size");
@@ -42,18 +48,18 @@ public class Fiber_Size extends PlugInFrame {
       return;
     } else {
       // Initialize instance variables
-      _imp = IJ.getImage();
+      orgImp = IJ.getImage();
+      bndImp = null;
       instance = this;
-      imp = null;
 
-      impLst = new ImageListener() {
-        public void imageClosed(ImagePlus imp_) {
-          if (imp_ == imp) imp = null;
+      imgLtn = new ImageListener() {
+        public void imageClosed(ImagePlus imp) {
+          if (imp == bndImp) bndImp = null;
         }
         public void imageOpened(ImagePlus imp_) {}
         public void imageUpdated(ImagePlus imp_) {}
       };
-      ImagePlus.addImageListener(impLst);
+      ImagePlus.addImageListener(imgLtn);
 
       // Update the window
       WindowManager.addWindow(this);
@@ -68,122 +74,134 @@ public class Fiber_Size extends PlugInFrame {
   // return the title bar height.
   public void addNotify() {
     super.addNotify();
-    int top = getInsets().top + 20;
-    Label label;
-    Button button;
 
-    // For the whole frame
-    Font normFont = new Font("Helvetica", Font.PLAIN, 12);
-    Font boldFont = new Font("Helvetica", Font.BOLD, 12);
-    setFont(normFont);
+    // Set defaults
+    setFont(new Font("Helvetica", Font.PLAIN, 12));
     setLayout(null);
     setResizable(false);
 
-    /*
-     * Step 1: Select Image
-     */
+    // Add controls
+    yPos = getInsets().top;
+    this.add_step1_controls()
+        .add_step2_controls()
+        .add_step3_controls()
+        .add_step4_controls()
+        .add_version_info();
+  }
+
+  // Step 1: Select Image
+  public Fiber_Size add_step1_controls() {
+    Label label;
+    Button button;
 
     label = new Label("Step 1: Select Image", Label.LEFT);
-    label.setFont(boldFont);
-    label.setBounds(10, top, WINDOW_WIDTH - 20, 14);
+    label.setFont(new Font("Helvetica", Font.BOLD, 12));
+    label.setBounds(10, yPos, WINDOW_WIDTH - 20, 14);
     add(label);
 
-    imgField = new TextField();
-    imgField.setEditable(false);
-    imgField.setText(_imp.getTitle());
-    imgField.setBounds(20, top + 25, 260, 24);
-    add(imgField);
+    imgFld = new TextField();
+    imgFld.setEditable(false);
+    imgFld.setText(orgImp.getTitle());
+    imgFld.setBounds(20, yPos + 25, 270, 24);
+    add(imgFld);
 
     button = new Button("Change to current");
-    button.setBounds(300, top + 25, 160, 24);
+    button.setBounds(300, yPos + 25, 160, 24);
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        if (imp != null) imp.close();
-        RoiManager roiMgr = RoiManager.getInstance();
-        if (roiMgr != null) { roiMgr.reset(); roiMgr.close(); }
-
-        _imp = IJ.getImage();
-        imgField.setText(_imp.getTitle());
+        closeAll();
+        orgImp = IJ.getImage();
+        imgFld.setText(orgImp.getTitle());
       }
     });
     add(button);
 
-    /*
-     * Step 2: Binary
-     */
+    yPos += 65;
+    return(this);
+  }
 
-    label = new Label("Step 2: Binary Boundry", Label.LEFT);
-    label.setFont(boldFont);
-    label.setBounds(10, top + 65, WINDOW_WIDTH - 20, 14);
+  // Step 2: Binary Fiber Boundary
+  public Fiber_Size add_step2_controls() {
+    Label label;
+    Button button;
+
+    label = new Label("Step 2: Binary Fiber Boundary", Label.LEFT);
+    label.setFont(new Font("Helvetica", Font.BOLD, 12));
+    label.setBounds(10, yPos, WINDOW_WIDTH - 20, 14);
     add(label);
 
-    label = new Label("Fiber Boundry", Label.LEFT);
-    label.setBounds(20, top + 95, 90, 14);
+    label = new Label("Threshold", Label.RIGHT);
+    label.setBounds(20, yPos + 30, 70, 14);
     add(label);
 
-    sclBar = new Scrollbar(Scrollbar.HORIZONTAL,
-      get_current_ip().getAutoThreshold(), 1, 1, 255);
-    sclBar.setBounds(120, top + 95, 100, 14);
-    sclBar.addAdjustmentListener(new AdjustmentListener() {
+    bndSclBar = new Scrollbar(Scrollbar.HORIZONTAL,
+      get_ip().getAutoThreshold(), 1, 1, 255);
+    bndSclBar.setBounds(100, yPos + 30, 120, 14);
+    bndSclBar.addAdjustmentListener(new AdjustmentListener() {
       public void adjustmentValueChanged(AdjustmentEvent e) {
-        thdField.setText(Integer.toString(sclBar.getValue()));
-        if (imp != null) update_binaryzation();
+        bndThdFld.setText(Integer.toString(bndSclBar.getValue()));
+        if (bndImp != null) update_binaryzation();
       }
     });
-    add(sclBar);
+    add(bndSclBar);
 
-    thdField = new TextField();
-    thdField.setEditable(false);
-    thdField.setText(Integer.toString(sclBar.getValue()));
-    thdField.setBounds(230, top + 90, 30, 24);
-    add(thdField);
+    bndThdFld = new TextField();
+    bndThdFld.setEditable(false);
+    bndThdFld.setText(Integer.toString(bndSclBar.getValue()));
+    bndThdFld.setBounds(230, yPos + 25, 30, 24);
+    add(bndThdFld);
 
     button = new Button("Binary");
-    button.setBounds(270, top + 90, 60, 24);
+    button.setBounds(270, yPos + 25, 60, 24);
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        if (imp != null) imp.close();
+        if (bndImp != null) bndImp.close();
         new_binaryzation();
       }
     });
     add(button);
 
     button = new Button("Erode");
-    button.setBounds(335, top + 90, 60, 24);
+    button.setBounds(335, yPos + 25, 60, 24);
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        if (imp == null) new_binaryzation();
-        ip.erode();
-        imp.updateAndDraw();
+        if (bndImp == null) new_binaryzation();
+        bndIp.erode();
+        bndImp.updateAndDraw();
       }
     });
     add(button);
 
     button = new Button("Dilate");
-    button.setBounds(400, top + 90, 60, 24);
+    button.setBounds(400, yPos + 25, 60, 24);
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        if (imp == null) new_binaryzation();
-        ip.dilate();
-        imp.updateAndDraw();
+        if (bndImp == null) new_binaryzation();
+        bndIp.dilate();
+        bndImp.updateAndDraw();
       }
     });
     add(button);
 
-    /*
-     * Step 3: Analyze Particles
-     */
+    yPos += 65;
+    return(this);
+  }
+
+  // Step 3: Analyze Particles
+  public Fiber_Size add_step3_controls() {
+    Label label;
+    Button button;
 
     label = new Label("Step 3: Analyze Particles", Label.LEFT);
-    label.setFont(boldFont);
-    label.setBounds(10, top + 125, WINDOW_WIDTH - 20, 14);
+    label.setFont(new Font("Helvetica", Font.BOLD, 12));
+    label.setBounds(10, yPos, WINDOW_WIDTH - 20, 14);
     add(label);
 
     button = new Button("Analyze");
-    button.setBounds(20, top + 150, 70, 24);
+    button.setBounds(20, yPos + 25, 70, 24);
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        if (imp == null) new_binaryzation();
+        if (bndImp == null) new_binaryzation();
         RoiManager roiMgr = RoiManager.getInstance();
         if (roiMgr != null) { roiMgr.reset(); roiMgr.close(); }
         analyze();
@@ -193,59 +211,180 @@ public class Fiber_Size extends PlugInFrame {
 
     label = new Label("Please select 'Add to Manager' in the following prompted dialog.", Label.LEFT);
     label.setFont(new Font("Helvetica", Font.PLAIN, 11));
-    label.setBounds(100, top + 155, WINDOW_WIDTH - 40, 14);
+    label.setBounds(100, yPos + 30, WINDOW_WIDTH - 40, 14);
     add(label);
 
-    label = new Label("Version 2.1.alpha (May 5, 2015)", Label.RIGHT);
+    yPos += 65;
+    return(this);
+  }
+
+  // Step 4: Select Fiber Type
+  public Fiber_Size add_step4_controls() {
+    Label label;
+    Button button;
+
+    label = new Label("Step 4: Select Fiber Type", Label.LEFT);
+    label.setFont(new Font("Helvetica", Font.BOLD, 12));
+    label.setBounds(10, yPos, WINDOW_WIDTH - 20, 14);
+    add(label);
+
+    label = new Label("Select by", Label.RIGHT);
+    label.setBounds(20, yPos + 30, 70, 14);
+    add(label);
+
+    chnChoice = new Choice();
+    chnChoice.setBounds(100, yPos + 28, 100, 18);
+    for (int i = 0; i <= orgImp.getNChannels(); i++) {
+      chnChoice.add(i == 0 ? "None" : "Channel " + Integer.toString(i));
+    }
+    chnChoice.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+          // Check whether we can measure
+          RoiManager roiMgr = RoiManager.getInstance();
+          if (roiMgr == null) return;
+          Roi[] rois = roiMgr.getRoisAsArray();
+          if (rois.length == 0) return;
+
+          // Close previous ones
+          ResultsTable result = ResultsTable.getResultsTable();
+          if (result != null) result.reset();
+          ij.text.TextWindow window = ResultsTable.getResultsWindow();
+          if (window != null) window.close();
+
+          if (chnChoice.getSelectedIndex() > 0)
+            measure();
+        }
+      }
+    });
+    add(chnChoice);
+
+    label = new Label("and by", Label.CENTER);
+    label.setBounds(210, yPos + 30, 50, 14);
+    add(label);
+
+    typChoice = new Choice();
+    typChoice.add("Mode");
+    typChoice.add("Mean");
+    typChoice.setBounds(270, yPos + 28, 60, 18);
+    typChoice.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        if (e.getStateChange() == ItemEvent.SELECTED) update_typSclBar();
+      }
+    });
+    add(typChoice);
+
+    label = new Label("Threshold", Label.RIGHT);
+    label.setBounds(20, yPos + 60, 70, 14);
+    add(label);
+
+    typSclBar = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, 1, 255);
+    typSclBar.setBounds(100, yPos + 60, 120, 14);
+    typSclBar.addAdjustmentListener(new AdjustmentListener() {
+      public void adjustmentValueChanged(AdjustmentEvent e) {
+        typThdFld.setText(Integer.toString(typSclBar.getValue()));
+        select_by_type("select");
+      }
+    });
+    add(typSclBar);
+
+    typThdFld = new TextField();
+    typThdFld.setEditable(false);
+    typThdFld.setText(Integer.toString(typSclBar.getValue()));
+    typThdFld.setBounds(230, yPos + 55, 30, 24);
+    add(typThdFld);
+
+    button = new Button("Select");
+    button.setBounds(270, yPos + 55, 60, 24);
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) { select_by_type("select"); }
+    });
+    add(button);
+
+    button = new Button("Reset");
+    button.setBounds(335, yPos + 55, 60, 24);
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) { select_by_type("reset"); }
+    });
+    add(button);
+
+    // button = new Button("Filter");
+    // button.setBounds(400, yPos + 55, 60, 24);
+    // button.addActionListener(new ActionListener() {
+    //   public void actionPerformed(ActionEvent e) { select_by_type("filter"); }
+    // });
+    // add(button);
+
+    yPos += 125;
+    return(this);
+  }
+
+  // Version info
+  public Fiber_Size add_version_info() {
+    Label label;
+
+    label = new Label("Version 2.1.beta (May 5, 2015)", Label.RIGHT);
     label.setFont(new Font("Helvetica", Font.PLAIN, 11));
     label.setBounds(10, WINDOW_HEIGHT-24, WINDOW_WIDTH - 20, 14);
     add(label);
+
+    return(this);
   }
 
-  /* Overrides windowClosing in PluginFrame. */
+  // Overrides windowClosing in PluginFrame
   public void windowClosing(WindowEvent e) {
     super.windowClosing(e);
-
-    // Close the imp
-    if (imp != null) imp.close();
-    ImagePlus.removeImageListener(impLst);
-
-    // For singleton
+    closeAll();
+    ImagePlus.removeImageListener(imgLtn);
     instance = null;
   }
 
-  // Get a copy of the current image in Gray8 format
-  public ImageProcessor get_current_ip() {
-    int oriC = _imp.getC();
-    _imp.setC(_imp.getNChannels());
-    ImageProcessor ip = _imp.getChannelProcessor().convertToByte(true);
-    _imp.setC(oriC);
+  // Close all windows
+  public void closeAll() {
+    if (bndImp != null) bndImp.close();
+    RoiManager roiMgr = RoiManager.getInstance();
+    if (roiMgr != null) { roiMgr.reset(); roiMgr.close(); }
+    ResultsTable result = ResultsTable.getResultsTable();
+    if (result != null) result.reset();
+    ij.text.TextWindow window = ResultsTable.getResultsWindow();
+    if (window != null) window.close();
+  }
+
+  // Get a copy of the working ImageProcessor
+  public ImageProcessor get_ip() {
+    return(get_ip(orgImp.getNChannels()));
+  }
+  public ImageProcessor get_ip(int c) {
+    int _c = orgImp.getC();
+    orgImp.setC(c);
+    ImageProcessor ip = orgImp.getChannelProcessor().convertToByte(true);
+    orgImp.setC(_c);
     return(ip);
   }
 
   // Create a binaryzation
   public void new_binaryzation() {
-    ip = get_current_ip();
-    ip.threshold(sclBar.getValue());
-    imp = new ImagePlus("Threshold : " + Integer.toString(sclBar.getValue()), ip);
-    imp.show();
-    imp.updateAndDraw();
+    bndIp = get_ip();
+    bndIp.threshold(bndSclBar.getValue());
+    bndImp = new ImagePlus("Threshold : " + Integer.toString(bndSclBar.getValue()), bndIp);
+    bndImp.show();
+    bndImp.updateAndDraw();
   }
 
   // Update the binaryzation
   public void update_binaryzation() {
-    ip = get_current_ip();
-    ip.threshold(sclBar.getValue());
-    imp.setProcessor(ip);
-    imp.setTitle("Threshold : " + Integer.toString(sclBar.getValue()));
+    bndIp = get_ip();
+    bndIp.threshold(bndSclBar.getValue());
+    bndImp.setProcessor(bndIp);
+    bndImp.setTitle("Threshold : " + Integer.toString(bndSclBar.getValue()));
   }
 
   // Analyze Particles
   public void analyze() {
     ParticleAnalyzer pa = new ParticleAnalyzer();
     if (!pa.showDialog()) return;
-    pa.analyze(imp, ip);
-    imp.close();
+    pa.analyze(bndImp, bndIp);
+    bndImp.close();
 
     // Rename
     RoiManager roiMgr = RoiManager.getInstance();
@@ -258,7 +397,95 @@ public class Fiber_Size extends PlugInFrame {
       roiMgr.addRoi(roi);
     }
 
-    roiMgr.runCommand(_imp, "Show All with labels");
+    roiMgr.runCommand(orgImp, "Show All with labels");
+  }
+
+  // Measure Rois
+  public void measure() {
+    ImagePlus imp = new ImagePlus(null, get_ip(chnChoice.getSelectedIndex()));
+    RoiManager roiMgr = RoiManager.getInstance();
+    roiMgr.runCommand(imp, "Deselect");
+    IJ.run("Set Measurements...", "mean modal min redirect=None decimal=1");
+    roiMgr.runCommand(imp, "Measure");
+    update_typSclBar();
+  }
+
+  // Update typSclBar
+  public void update_typSclBar() {
+    ResultsTable result = ResultsTable.getResultsTable();
+    if (result == null) return;
+
+    double[] vals;
+    switch (typChoice.getSelectedIndex()) {
+      case 0: vals = result.getColumnAsDoubles(ResultsTable.MODE); break;
+      case 1: vals = result.getColumnAsDoubles(ResultsTable.MEAN); break;
+      default: vals = null; break;
+    }
+    if (vals == null) return;
+
+    typSclBar.setMinimum((int)Math.floor(vals[0]));
+    typSclBar.setMaximum((int)Math.floor(vals[0]) + 1);
+    for (int i = 1; i < vals.length; i++) {
+      if(vals[i] < typSclBar.getMinimum())
+        typSclBar.setMinimum((int)Math.floor(vals[i]));
+      if(vals[i] > typSclBar.getMaximum())
+        typSclBar.setMaximum((int)Math.floor(vals[i]) + 1);
+    }
+    typSclBar.setValue((int)(typSclBar.getMinimum() + typSclBar.getMaximum()) / 2);
+    typThdFld.setText(Integer.toString(typSclBar.getValue()));
+    select_by_type("select");
+  }
+
+  // Selct Rois by fiber type
+  public Roi[] allRois;
+  public void select_by_type(String param) {
+    RoiManager roiMgr = RoiManager.getInstance();
+    if (roiMgr == null) return;
+    ResultsTable result = ResultsTable.getResultsTable();
+    if (result == null) return;
+
+    double[] vals;
+    switch (typChoice.getSelectedIndex()) {
+      case 0: vals = result.getColumnAsDoubles(ResultsTable.MODE); break;
+      case 1: vals = result.getColumnAsDoubles(ResultsTable.MEAN); break;
+      default: vals = null; break;
+    }
+    if (vals == null) return;
+    int threshold = Integer.parseInt(typThdFld.getText());
+
+    Roi[] rois = roiMgr.getRoisAsArray();
+    if (rois.length == vals.length)
+      allRois = rois;
+    else
+      rois = allRois;
+
+    // It begins!
+    roiMgr.reset();
+    for(int i = 0; i < rois.length; i++) {
+      Roi roi = rois[i];
+      if (vals[i] > threshold) {
+        if (param.equals("select")) {
+          roi.setLineWidth(3);
+          roi.setStrokeColor(Color.YELLOW);
+        } else if (param.equals("reset")) {
+          roi.setLineWidth(1);
+          roi.setStrokeColor(Color.YELLOW);
+        }
+        roiMgr.addRoi(roi);
+      } else {
+        if (param.equals("reset")){
+          roi.setLineWidth(1);
+          roi.setStrokeColor(Color.YELLOW);
+          roiMgr.addRoi(roi);
+        }
+      }
+    }
+
+    if (param.equals("select")) {
+      roiMgr.runCommand(orgImp, "Show All without labels");
+    } else if (param.equals("reset")) {
+      roiMgr.runCommand(orgImp, "Show All with labels");
+    }
   }
 
 }
